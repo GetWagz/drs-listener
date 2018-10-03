586370dd49b9fcd2f7c9d220d1fd7b403dd538fc
Kevin Eaton <kevinhowardeaton@gmail.com>
2018-10-03 12:01:22 -0400

    added deregister hook

--- src/router.test.ts
+++ src/router.test.ts
@@ -1,14 +1,42 @@
-import { receiveRequest } from "./";
+import { IHandlers, receiveRequest } from "./";
 
 import { deviceDeregisteredNotification } from "./router.test.messages";
 
-test("test incoming parsed body", () => {
-  receiveRequest(deviceDeregisteredNotification, (err, validated) => {
-    expect(err).toBeNull();
-    expect(validated).toBeTruthy();
-    expect(validated).toHaveProperty("Message");
-    expect(validated.Subject).toEqual(deviceDeregisteredNotification.Subject);
-    const message = validated.Message;
-    expect(message).toBeTruthy();
-  });
+// const handlers: IHandlers = {
+//   onError: (error) => {
+//     console.log(error);
+//   },
+//   onDeviceDeregistered: (error, result) => {
+//     console.log("done");
+//   }
+// };
+
+// test("test incoming parsed body", () => {
+//   receiveRequest(deviceDeregisteredNotification, handlers, (err, validated) => {
+//     expect(err).toBeNull();
+//     expect(validated).toBeTruthy();
+//     expect(validated).toHaveProperty("Message");
+//     expect(validated.Subject).toEqual(deviceDeregisteredNotification.Subject);
+//     const message = validated.Message;
+//     expect(message).toBeTruthy();
+//   });
+// });
+test("test incoming parsed body", (done) => {
+  const h: IHandlers = {
+    onError: (e: any) => {
+      // this should not be called
+      expect(e).toBeNull();
+      done();
+    },
+    onDeviceDeregistered: (customerId: string, modelId: string, serialNumber: string, message: any) => {
+      expect(customerId).toEqual("amzn1.account.FAKETEST");
+      expect(modelId).toEqual("Testing_DRS_Testing");
+      expect(serialNumber).toEqual("1122334455");
+      expect(message).not.toBeNull();
+      expect(message).not.toEqual({});
+      done();
+    }
+  };
+  receiveRequest(deviceDeregisteredNotification, h);
+  
 });
